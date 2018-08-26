97757aae97b29451f3b02d761bcef00ea006a5a4
Tilman Stehr <tilman@tilman.ninja>
2018-08-26 14:40:19 +0200

    Make example a python package

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="recipemd",
+    version="1.0.0",
+    author="AberDerBart, Tilman Stehr",
+    author_email="tilman@tilman.ninja",
+    description="Reference implementation of recipemd",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tstehr/recipemd",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7,<4',
+    install_requires=[
+       'CommonMark',
+       'commonmarkextensions'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent",
+    ],
+)
